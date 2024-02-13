a71ebdc2561aad170fc9b490bfead66400e8e598
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-02-13 13:42:20 -0300

    funcionario-form

--- project_employee/src/app/app.module.ts
+++ project_employee/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
+import {FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +20,9 @@ import { FuncionarioFormComponent } from './componentes/funcionario-form/funcion
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project_employee/src/app/componentes/funcionario-form/funcionario-form.component.ts
+++ project_employee/src/app/componentes/funcionario-form/funcionario-form.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-funcionario-form',
   templateUrl: './funcionario-form.component.html',
   styleUrls: ['./funcionario-form.component.css']
 })
-export class FuncionarioFormComponent {
+export class FuncionarioFormComponent implements OnInit {
+
+  funcionarioForm!: FormGroup;
+
+  constructor (){}
+
+  ngOnInit(): void {
+    this.funcionarioForm = new FormGroup({
+      nome: new FormControl('')
+
+    });
+  }
+  submit(){
+
+  }
 
 }
