b7b99c88d094dd0a2512ad3d7fc2846c317d1829
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-03-21 07:37:47 -0300

    cypress-video

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   port:4021,
   e2e: {
-    'baseUrl': 'http://localhost:4200',
-  },
+    baseUrl: 'http://localhost:4200',
+    video:true,
 
+  },
 
   component: {
     devServer: {
